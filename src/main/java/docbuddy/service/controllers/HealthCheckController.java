6ab09494403d76ca9c591b9ad7a70d2e66350ff4
nbantar <visperadel2deabril>
2018-09-05 15:08:06 -0300

    Develop a first REST service with just a health check.

---
+++ src/main/java/docbuddy/service/controllers/HealthCheckController.java
@@ -0,0 +1,15 @@
+package docbuddy.service.controllers;
+
+import docbuddy.service.responses.HealthCheckResponse;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @RequestMapping("/healthcheck")
+    public HealthCheckResponse healthCheck(@RequestParam(value = "name", defaultValue = "Dr.") String name) {
+        return new HealthCheckResponse(String.format("Service is up %s!!", name));
+    }
+}
---
+++ src/main/java/docbuddy/service/responses/HealthCheckResponse.java
@@ -0,0 +1,14 @@
+package docbuddy.service.responses;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class HealthCheckResponse {
+    private String description;
+}
