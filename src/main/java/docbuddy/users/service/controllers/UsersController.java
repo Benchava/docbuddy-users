4b1ead2d4ccb69c86f3a0294253c01d5e0f4338b
nestor <nestorgf@gmail.com>
2018-09-25 14:53:33 -0300

    Removed unused imports
    Added custom exception to handle invalid credentials on login

--- src/main/java/docbuddy/users/service/controllers/UsersController.java
+++ src/main/java/docbuddy/users/service/controllers/UsersController.java
@@ -6,7 +6,6 @@ import docbuddy.users.service.UserService;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
