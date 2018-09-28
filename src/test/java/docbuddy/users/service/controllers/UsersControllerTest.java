60647277071373e83dda5a8360c8a365d1da90ef
nestor <nestorgf@gmail.com>
2018-09-28 17:58:36 -0300

    Added tests assertions

--- src/test/java/docbuddy/users/service/controllers/UsersControllerTest.java
+++ src/test/java/docbuddy/users/service/controllers/UsersControllerTest.java
@@ -4,22 +4,22 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import docbuddy.users.service.AuthService;
 import docbuddy.users.service.request.LoginRequest;
 import docbuddy.users.service.responses.LoginResponse;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
@@ -43,13 +43,16 @@ public class UsersControllerTest {
 
         when(authService.login(any())).thenReturn(response);
 
-        mvc.perform(post("/auth/login")
+        MockHttpServletResponse controllerResponse = mvc.perform(post("/auth/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .characterEncoding("UTF-8")
                 .content(mapper.writeValueAsString(request)))
                 .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.token", is(response.getToken())));
+                .andReturn().getResponse();
 
+
+        Assert.assertEquals(controllerResponse.getStatus(), HttpStatus.OK.value());
+        Assert.assertEquals(controllerResponse.getContentAsString(),
+                mapper.writeValueAsString(response));
     }
 }
