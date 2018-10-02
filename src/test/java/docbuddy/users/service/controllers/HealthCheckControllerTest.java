6130802f60a09128151a3baaa4e8b4222ee80e95
nbantar <nicobantar87@gmail.com>
2018-10-02 11:33:06 -0300

    Added Unit Tests

--- src/test/java/docbuddy/users/service/controllers/HealthCheckControllerTest.java
+++ src/test/java/docbuddy/users/service/controllers/HealthCheckControllerTest.java
@@ -2,13 +2,16 @@ package docbuddy.users.service.controllers;
 
 import docbuddy.users.BaseControllerTest;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
 @WebMvcTest(HealthCheckController.class)
 public class HealthCheckControllerTest extends BaseControllerTest {
 
@@ -17,7 +20,7 @@ public class HealthCheckControllerTest extends BaseControllerTest {
     @Test
     public void healthCheckNoNameSuccess() throws Exception {
         final String DEFAULT_NAME = "Dr.";
-        mockMvc.perform(get("/healthcheck"))
+        mvc.perform(get("/healthcheck"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.description", is(String.format(DEFAULT_RESPONSE, DEFAULT_NAME))));
     }
@@ -25,7 +28,7 @@ public class HealthCheckControllerTest extends BaseControllerTest {
     @Test
     public void healthCheckWithNameSuccess() throws Exception {
         final String TEST_NAME = "John";
-        mockMvc.perform(get(String.format("/healthcheck?name=%s", TEST_NAME)))
+        mvc.perform(get(String.format("/healthcheck?name=%s", TEST_NAME)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.description", is(String.format(DEFAULT_RESPONSE, TEST_NAME))));
     }
