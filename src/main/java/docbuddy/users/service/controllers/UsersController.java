c7998a581db4af26c18428b3cd51ea6498868621
nestor <nestorgf@gmail.com>
2018-10-04 15:41:46 -0300

    removed unused imports

--- src/main/java/docbuddy/users/service/controllers/UsersController.java
+++ src/main/java/docbuddy/users/service/controllers/UsersController.java
@@ -1,12 +1,7 @@
 package docbuddy.users.service.controllers;
 
-import docbuddy.users.model.User;
-import docbuddy.users.persistence.Result;
-import docbuddy.users.service.UserService;
-import docbuddy.users.service.request.LoginRequest;
-import docbuddy.users.service.responses.LoginResponse;
-import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,45 +9,49 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.SQLException;
+import docbuddy.users.model.User;
+import docbuddy.users.persistence.Result;
+import docbuddy.users.service.UserService;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
 @NoArgsConstructor
 public class UsersController {
-    @Autowired
-    private UserService userService;
+	@Autowired
+	private UserService userService;
 
-    @RequestMapping(value = "/add", method = RequestMethod.PUT)
-    public Long addUser(@RequestBody User user) {
-        log.info("About to insert object in Firebase.");
-        return userService.createUser(user);
-    }
+	@RequestMapping(value = "/add", method = RequestMethod.PUT)
+	public Long addUser(@RequestBody User user) {
+		log.info("About to insert object in Firebase.");
+		return userService.createUser(user);
+	}
 
-    @RequestMapping(value = "/get", method = RequestMethod.GET)
-    public User getUser(@RequestParam Long id) throws SQLException {
-        return userService.getUser(id);
-    }
+	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	public User getUser(@RequestParam Long id) throws SQLException {
+		return userService.getUser(id);
+	}
 
-    @RequestMapping(value = "/get/all", method = RequestMethod.GET)
-    public Result<User> getAllUser() throws SQLException {
-        return userService.getAllUsers();
-    }
+	@RequestMapping(value = "/get/all", method = RequestMethod.GET)
+	public Result<User> getAllUser() throws SQLException {
+		return userService.getAllUsers();
+	}
 
-    @RequestMapping(value = "/update", method = RequestMethod.PATCH)
-    public void updateUser(@RequestBody User updatedUser) {
-        log.info("About to update object in Firebase.");
-        userService.updateUser(updatedUser);
-    }
+	@RequestMapping(value = "/update", method = RequestMethod.PATCH)
+	public void updateUser(@RequestBody User updatedUser) {
+		log.info("About to update object in Firebase.");
+		userService.updateUser(updatedUser);
+	}
 
-    @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
-    public void deleteUser(@RequestParam Long id) {
-        userService.deleteUser(id);
-    }
-    
-    @RequestMapping(value = "/find", method = RequestMethod.POST) 
-    public User checkUser(@RequestBody User userRequest) {
-    	return userService.getUser(userRequest);
-    	
-    }
+	@RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+	public void deleteUser(@RequestParam Long id) {
+		userService.deleteUser(id);
+	}
+
+	@RequestMapping(value = "/find", method = RequestMethod.POST)
+	public User checkUser(@RequestBody User userRequest) {
+		return userService.getUser(userRequest);
+
+	}
 }
