54ca31564356ab08ce76e68b577b001f6d8bfcba
nestor <nestorgf@gmail.com>
2018-10-04 15:38:27 -0300

    test and proyect fix

--- src/main/java/docbuddy/users/service/AuthService.java
+++
@@ -1,9 +0,0 @@
-package docbuddy.users.service;
-
-import docbuddy.users.service.request.LoginRequest;
-import docbuddy.users.service.responses.LoginResponse;
-
-public interface AuthService {
-
-    public LoginResponse login(LoginRequest request);
-}
